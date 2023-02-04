492040cff2cb3ca4de52a97618390c66e3288210
eliyya <tecno_eli@hotmail.com>
2023-02-04 16:52:21 -0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
